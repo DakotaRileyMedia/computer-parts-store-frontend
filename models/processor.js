f5ae21b406a63c3d21076f6da1e64cd871403db5
Dakota <dakotacustompcs@gmail.com>
2024-02-29 12:22:23 -0500

    Modified our processor model and _form_fields to show more options

--- models/processor.js
+++ models/processor.js
@@ -5,6 +5,55 @@ const processorSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  // description: {
+  //   type: String,
+  // },
+  // brand: {
+  //   type: String,
+  //   required: true,
+  // },
+  // socket: {
+  //   type: String,
+  // },
+  // processorGeneration: {
+  //   type: Number,
+  // },
+  // processorClass: {
+  //   type: String,
+  // },
+  // coreCount: {
+  //   type: Number,
+  // },
+  // threadCount: {
+  //   type: Number,
+  // },
+  // baseClock: {
+  //   type: Number,
+  // },
+  // maxTurboClock: {
+  //   type: Number,
+  // },
+  // l1Cache: {
+  //   type: String,
+  // },
+  // l2Cache: {
+  //   type: String,
+  // },
+  // l3Cache: {
+  //   type: String,
+  // },
+  // tdp: {
+  //   type: Number,
+  // },
+  // integratedGraphics: {
+  //   type: Boolean,
+  // },
+  // coolerIncluded: {
+  //   type: Boolean,
+  // },
+  // productImage: {
+  //   type: String,
+  // },
 });
 
 module.exports = mongoose.model('Processor', processorSchema);
